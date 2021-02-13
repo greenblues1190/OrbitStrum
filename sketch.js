b4fd6b2c45f3ab1c632e755957e837d0a0068f20
Jeongmin Woo <greenblues1190@gmail.com>
2021-02-14 04:12:12 +0900

    responsive elements, Hz, intro message, spacebar input added

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ const midiNotes = [
   ["C4", "E4", "G4", "C5", "G4", "E4", "C4", "G3"],
 ];
 
-let createNoteNumTable = () => {
+function createNoteNumTable() {
   let root;
   let noteNumTable = [];
   for (let oct = 0; oct < 8; ++oct) {
@@ -56,24 +56,27 @@ let createNoteNumTable = () => {
   }
 
   return noteNumTable;
-};
+}
 
 const noteNumTable = createNoteNumTable();
 
-let noteToNum = (note) => {
+function noteToNum(note) {
   if (!note) return 0;
   const regexp = /([A-G]#?)(\d)/;
   parsedNote = regexp.exec(note);
   let key = parsedNote[1];
   let oct = parsedNote[2];
   return noteNumTable[oct][key];
-};
+}
 
-const fps = 30;
+const fps = 60;
+const X_AXIS = 1;
+const Y_AXIS = 2;
 
-let bg;
-let lightGray;
+let myFont;
+let bgBottom, bgTop, ballColor, lineColor, textColor;
 
+let intro;
 let newGrid;
 let ballList = [];
 let lineList = [];
@@ -81,113 +84,137 @@ let lineList = [];
 let isPlaying = false;
 let bar = 0;
 
-let viewScale = 1;
+let viewScale;
 let ballAmount = 2;
-let velocity = 12;
+let velocity = 7;
 
-function Ball(startX, startY, rad, orbitRad, angle, velocity) {
-  this.startX = startX;
-  this.startY = startY;
-  this.x = width / 2;
-  this.y = height / 2;
+function Ball(ballColor, rad, angle, velocity, clockwise = false) {
+  this.color = color(ballColor);
+  this.targetVec = createVector(0, 0);
+  this.vec = createVector((width / 5) * (clockwise ? -1 : 1), 0);
   this.r = rad;
-  this.scalar = orbitRad;
   this.angle = angle;
   this.velocity = velocity;
-  this.ease = 0.3;
-
+  this.clockwise = clockwise;
+  this.ease = 0.5;
   this.display = function () {
-    noStroke();
-    fill(lightGray);
-    circle(this.x, this.y, this.r * 2);
+    // noStroke();
+    stroke(color("#FFFFFF"))
+    fill(this.color);
+    circle(this.vec.x + width / 2, this.vec.y + height / 2, this.r * 2);
   };
 
   this.update = function () {
-    const centerX = width / 2;
-    const centerY = height / 2;
-    let targetX =
-      centerX +
-      this.startX +
-      this.scalar * viewScale * cos(radians(this.angle));
-    let dx = targetX - this.x;
-    this.x += dx * this.ease;
+    let orbitRadius = width / 4;
 
-    let targetY =
-      centerY +
-      this.startY +
-      this.scalar * viewScale * sin(radians(this.angle));
-    let dy = targetY - this.y;
-    this.y += dy * this.ease;
+    let startVec = createVector(orbitRadius * (this.clockwise ? -1 : 1), 0);
 
-    this.angle += 0.1 * this.velocity;
+    this.targetVec.x = orbitRadius * cos(radians(this.angle));
+    this.targetVec.y = orbitRadius * sin(radians(this.angle));
+    this.targetVec.add(startVec).mult(viewScale);
+
+    this.vec = easeVector(this.vec, this.targetVec, this.ease);
+
+    this.angle += (this.clockwise ? 1 : -1) * 0.1 * this.velocity; //
   };
 }
 
-function Line(y) {
-  this.targetVec1 = createVector(0, y);
-  this.targetVec2 = createVector(0, y);
-  this.vec1 = createVector(width / 2, y + height / 2);
-  this.vec2 = createVector(width / 2, y + height / 2);
+function Line(quantity, index, lineColor) {
+  this.quantity = quantity;
+  this.index = index;
+  this.lineSpace = 62;
+  this.targetVec1 = createVector(0, 0);
+  this.targetVec2 = createVector(0, 0);
+  let y = ((this.lineSpace * width) / 1440) * (this.index - (this.quantity - 1) / 2);
+  this.vec1 = createVector(
+    0,
+    y
+  );
+  this.vec2 = createVector(
+    0,
+    y
+  );
   this.cpx = (this.vec1.x + this.vec2.x) / 2;
   this.cpy = (this.vec1.y + this.vec2.y) / 2;
   this.shakyCpx = this.cpx;
   this.shakyCpy = this.cpy;
 
-  this.ease = 0.3;
+  this.ease = 0.2;
 
-  this.color = lightGray;
+  this.color = color(lineColor);
   this.magnitude = 0;
   this.tail = 0.5;
 
   this.osc = new p5.Oscillator("sine");
   this.osc.amp(0);
-  this.env = new p5.Envelope(0.0045, velocity * 0.01, 0.2 + this.tail / 1.8, 0);
+  this.env = new p5.Envelope(0.0045, velocity * 0.05, 0.2 + this.tail / 1.8, 0);
   this.osc.start();
   reverb.process(this.osc, 5, 2);
 
-  this.isHit = false;
+  this.isFirstHit = false;
 
   this.display = function () {
+    const centerVec = createVector(width / 2, height / 2);
+
     noFill();
     stroke(this.color);
     bezier(
-      this.vec1.x,
-      this.vec1.y,
-      this.shakyCpx,
-      this.shakyCpy,
-      this.shakyCpx,
-      this.shakyCpy,
-      this.vec2.x,
-      this.vec2.y
+      this.vec1.x + centerVec.x,
+      this.vec1.y + centerVec.y,
+      this.shakyCpx + centerVec.x,
+      this.shakyCpy + centerVec.y,
+      this.shakyCpx + centerVec.x,
+      this.shakyCpy + centerVec.y,
+      this.vec2.x + centerVec.x,
+      this.vec2.y + centerVec.y
+    );
+    let stringLength = dist(this.vec1.x, this.vec1.y, this.vec2.x, this.vec2.y);
+    let lengthToFreq =
+      -1 * ((stringLength / viewScale / (width / 1440)) * 1.5 - 1300);
+
+    fill(this.color);
+    noStroke();
+    textSize(12);
+    text(
+      stringLength > 1 ? lengthToFreq.toFixed(2) + " Hz" : "",
+      this.vec2.x + centerVec.x + 50 / (1440 / width),
+      this.vec2.y + centerVec.y + 5
     );
   };
 
-  this.checkCollision = function (ball) {
+  this.checkCollision = function (ballList) {
     // check for collision
     // if hit, change line's stroke color
-    let hit = lineCircle(this, ball);
-    if (hit && this.isHit === false) {
-      this.magnitude = abs(ball.velocity / 1.5);
+    let hitBallIndex = false;
+    ballList.map((ball, index) => {
+      let hit = lineCircle(this, ball);
+      if (hit) {
+        hitBallIndex = index;
+      }
+    });
+    if (hitBallIndex !== false && !this.isFirstHit) {
+      this.magnitude = abs(ballList[hitBallIndex].velocity * 3);
       let stringLength = dist(
         this.vec1.x,
         this.vec1.y,
         this.vec2.x,
         this.vec2.y
       );
-      let lengthToFreq = -1 * ((stringLength - 50) * 3 - 1024);
+      let lengthToFreq =
+        -1 * ((stringLength / viewScale / (width / 1440)) * 1.5 - 1300);
       this.osc.freq(lengthToFreq);
-      if (stringLength > 50) {
+      if (stringLength > 30) {
         this.env.play(this.osc);
       }
-      this.isHit = true;
-    } else if (!hit && this.isHit) {
-      this.isHit = false;
+      this.isFirstHit = true;
+    } else if (hitBallIndex === false && this.isFirstHit) {
+      this.isFirstHit = false;
     }
   };
 
   this.changeNote = function (note) {
     let x1, x2, stringLength;
-    stringLength = note > 0 ? (1024 - midiToFreq(note)) / 3 + 50 : 0;
+    stringLength = note > 0 ? (1300 - midiToFreq(note)) / 1.5 + 0 : 0;
     x1 = (-1 * stringLength) / 2;
     x2 = stringLength / 2;
 
@@ -203,25 +230,43 @@ function Line(y) {
   };
 
   this.update = function () {
-    const centerVec = createVector(width / 2, height / 2);
-    let absoluteTargetVec1 = p5.Vector.add(this.targetVec1, centerVec);
-    let absoluteTargetVec2 = p5.Vector.add(this.targetVec2, centerVec);
+    let lineSpace = this.lineSpace * (width / 1440);
+
+    let y = lineSpace * (this.index - (this.quantity - 1) / 2);
+    this.targetVec1.y = y;
+    this.targetVec2.y = y;
+
+    let absoluteTargetVec1 = createVector(
+      this.targetVec1.x * (width / 1440),
+      this.targetVec1.y
+    );
+    absoluteTargetVec1.mult(viewScale);
+
+    let absoluteTargetVec2 = createVector(
+      this.targetVec2.x * (width / 1440),
+      this.targetVec2.y
+    );
+    absoluteTargetVec2.mult(viewScale);
+
     this.vec1 = easeVector(this.vec1, absoluteTargetVec1, this.ease);
+
     this.vec2 = easeVector(this.vec2, absoluteTargetVec2, this.ease);
 
     this.cpx = (this.vec1.x + this.vec2.x) / 2;
     this.cpy = (this.vec1.y + this.vec2.y) / 2;
 
-    if (this.magnitude > 0) this.magnitude *= 0.8 + this.tail * 0.13;
+    if (this.magnitude > 0) {
+      this.magnitude *= 0.8 + this.tail * 0.2;
+    }
     this.shakyCpx = this.cpx;
-    this.shakyCpy = this.cpy + sin(frameCount * 1.5) * this.magnitude;
+    this.shakyCpy =
+      this.cpy + (sin(frameCount * 1.5) * this.magnitude * width) / 1440;
   };
 }
 
-function grid(gridColor) {
-  this.gridColor = gridColor;
+function Grid(gridColor) {
+  this.gridColor = color(gridColor);
   this.centerVec = createVector(width / 2, height / 2);
-  this.ease = 0.3;
 
   this.display = function () {
     stroke(this.gridColor);
@@ -229,14 +274,44 @@ function grid(gridColor) {
     line(0, this.centerVec.y, width, this.centerVec.y);
   };
   this.update = function () {
-    const updatedCenterVec = createVector(width / 2, height / 2);
-    this.centerVec = easeVector(this.centerVec, updatedCenterVec, this.ease);
+    this.centerVec.x = width / 2;
+    this.centerVec.y = height / 2;
   };
 }
 
-// function preload() {
-//   inconsolata = loadFont("assets/SourceSansPro-Regular.ttf");
-// }
+function Intro(introColor) {
+  this.title = "orbit strum";
+  this.message = "click or press spacebar";
+  this.titleColor = color(introColor);
+  this.messageColor = color(introColor);
+
+  this.display = function () {
+    noStroke();
+    textSize(32);
+    fill(this.titleColor);
+    let introWidth = textWidth(this.title);
+    text(this.title, width / 2 - introWidth / 2, height / 5 - 12);
+    textSize(18);
+    fill(this.messageColor);
+    introWidth = textWidth(this.message);
+    text(this.message, width / 2 - introWidth / 2, height / 2 + 9);
+  };
+  this.update = function () {
+    if (isPlaying) {
+      let introAlpha = alpha(this.titleColor);
+      if (introAlpha > 0) {
+        this.titleColor.setAlpha(alpha(this.titleColor) - 15);
+        this.messageColor.setAlpha(alpha(this.messageColor) - 15);
+      }
+    } else {
+      this.messageColor.setAlpha(128 + 128 * sin(frameCount / fps * 3));
+    }
+  };
+}
+
+function preload() {
+  myFont = loadFont('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/IBMPlexSansKR-Medium.woff'); 
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -246,101 +321,125 @@ function setup() {
   let dryWet = 0.6;
   reverb.drywet(dryWet);
 
-  bg = color("#1D4E89");
-  lightGray = color("#e6e6e6");
+  textFont(myFont);
 
-  newGrid = new grid(color("#00397F"));
+  bgTop = color("#FF6F03");
+  bgBottom = color("#FF6F03");
+  let colorA = "#f4f9f9";
+  let black = "#000000";
+  ballColor = black;
+  lineColor = colorA;
+  textColor = colorA;
+  gridColor = "#3f72af"
 
-  let orbitRadius = width / 4;
-  for (let i = 0; i < ballAmount; i += 1) {
-    let newBall = new Ball(
-      -1 * orbitRadius,
-      0,
-      4,
-      orbitRadius,
-      (360 / ballAmount) * i - 50,
-      velocity
-    );
-    ballList.push(newBall);
-  }
-  for (let i = 0; i < ballAmount; i += 1) {
-    let newBall = new Ball(
-      orbitRadius,
-      0,
-      4,
-      orbitRadius,
-      (360 / ballAmount) * (i + 0.5) + 50,
-      -1 * velocity
-    );
-    ballList.push(newBall);
-  }
+  viewScale = 0.9;
 
-  let lineAmount = 8;
-  let lineSpace = 37;
-  for (let i = 0; i < lineAmount; i += 1) {
-    newLine = new Line(lineSpace * ((lineAmount - 1) / 2 - i) * viewScale);
+  newGrid = new Grid(gridColor);
+
+  let lineQuantity = midiNotes[0].length;
+  for (let i = 0; i < lineQuantity; i += 1) {
+    newLine = new Line(lineQuantity, i, lineColor);
     lineList.push(newLine);
   }
 
-  textFont("assets/SourceSansPro-Regular.ttf");
-  textSize(32);
+  let ballRad = 6;
+
+  for (let i = 0; i < ballAmount; i += 1) {
+    let newBall = new Ball(ballColor, ballRad, (360 / ballAmount) * (i + 0.5) - 130, velocity);
+    ballList.push(newBall);
+  }
+
+  for (let i = 0; i < ballAmount; i += 1) {
+    let newBall = new Ball(ballColor, ballRad, (360 / ballAmount) * i - 50, velocity, true);
+    ballList.push(newBall);
+  }
+
+  intro = new Intro(textColor);
 
   // mimics the autoplay policy
   getAudioContext().suspend();
 }
 
 function draw() {
-  background(bg);
+  setGradient(0, 0, width, height, bgTop, bgBottom, Y_AXIS)
 
-  newGrid.update();
-  newGrid.display();
+  // newGrid.update();
+  // newGrid.display();
 
-  fill(lightGray);
+  lineList.map((line) => {
+    line.checkCollision(ballList);
+    line.update();
+    line.display();
+  });
 
   ballList.map((ball) => {
     ball.update();
     ball.display();
   });
 
-  lineList.map((line) => {
-    ballList.map((ball) => {
-      line.checkCollision(ball);
-    });
-    line.update();
-    line.display();
-  });
+  
 
   // 1 strum = 1초 / 1 strum에 걸리는 시간 = (frameCount / fps) / (((180 / ballAmount)/(0.1 * velocity))/fps)
-  let deg = int((frameCount * 0.1 * velocity * ballAmount) / 2); // 1 strum * 90
-  let strum = int(deg / 90);
+  // let deg = int((frameCount * 0.1 * velocity * ballAmount) / 2); // 1 strum * 90
+  // let strum = int(deg / 90);
+  // fill(color(textColor));
+  // noStroke();
+  // textSize(32);
   // text("strum " + strum + " deg " + deg, 10, 30);
-  if (deg % 90 === 0 && isPlaying) {
-    changeMelody(strum);
+  // if (deg % 90 === 0 && isPlaying) {
+  //   changeMelody(strum);
+  // }
+
+  intro.update();
+  intro.display();
+}
+
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+
+  if (axis === Y_AXIS) {
+    // 위에서 아래 방향 그래디언트
+    for (let i = y; i <= y + h; i++) {
+      let inter = map(i, y, y + h, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x + w, i);
+    }
+  } else if (axis === X_AXIS) {
+    // 왼쪽에서 오른쪽 방향 그래디언트
+    for (let i = x; i <= x + w; i++) {
+      let inter = map(i, x, x + w, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(i, y, i, y + h);
+    }
   }
 }
 
 // this function fires after the mouse has been
 // clicked anywhere
 function mousePressed() {
-  if (isPlaying) {
-    lineList.map((line, index) => {
-      line.changeNote(0);
-    });
-  } else {
-    lineList.map((line, index) => {
-      note = 56;
-      line.changeNote(note);
-    });
+  if (!isPlaying) {
+    isPlaying = true;
+    userStartAudio();
   }
-  isPlaying = !isPlaying;
-  userStartAudio();
+  changeMelody(int(random(midiNotes.length)));
 }
 
-function changeMelody(strum) {
-  // console.log(strum);
-  let note;
+function keyPressed() {
+  if (keyCode === 32) {
+    if (!isPlaying) {
+      isPlaying = true;
+      userStartAudio();
+    }
+    changeMelody(int(random(midiNotes.length)));
+  }
+  return false; // prevent any default behaviour
+}
+
+function changeMelody(chordIndex) {
   lineList.map((line, index) => {
-    note = midiNotes[strum % midiNotes.length][index];
+    note = midiNotes[chordIndex][index];
     line.changeNote(noteToNum(note));
   });
 }
@@ -354,7 +453,7 @@ function windowResized() {
 }
 
 // LINE/CIRCLE
-let lineCircle = (line, circle) => {
+function lineCircle(line, circle) {
   // is either end INSIDE the circle?
   // if so, return true immediately
 
@@ -362,8 +461,8 @@ let lineCircle = (line, circle) => {
   let y1 = line.vec1.y;
   let x2 = line.vec2.x;
   let y2 = line.vec2.y;
-  let cx = circle.x;
-  let cy = circle.y;
+  let cx = circle.vec.x;
+  let cy = circle.vec.y;
   let r = circle.r;
 
   let inside1 = pointCircle(x1, y1, cx, cy, r);
@@ -387,12 +486,6 @@ let lineCircle = (line, circle) => {
   let onSegment = linePoint(x1, y1, x2, y2, closestX, closestY);
   if (!onSegment) return false;
 
-  // optionally, draw a circle at the closest
-  // point on the line
-  // fill(255, 0, 0);
-  // noStroke();
-  // ellipse(closestX, closestY, 10, 10);
-
   // get distance to closest point
   let distance = dist(closestX, closestY, cx, cy);
 
@@ -400,10 +493,10 @@ let lineCircle = (line, circle) => {
     return true;
   }
   return false;
-};
+}
 
 // POINT/CIRCLE
-let pointCircle = (px, py, cx, cy, r) => {
+function pointCircle(px, py, cx, cy, r) {
   // get distance between the point and circle's center
   // using the Pythagorean Theorem
   let distance = dist(px, py, cx, cy);
@@ -414,10 +507,10 @@ let pointCircle = (px, py, cx, cy, r) => {
     return true;
   }
   return false;
-};
+}
 
 // LINE/POINT
-let linePoint = (x1, y1, x2, y2, px, py) => {
+function linePoint(x1, y1, x2, y2, px, py) {
   // get distance from the point to the two ends of the line
   let d1 = dist(px, py, x1, y1);
   let d2 = dist(px, py, x2, y2);
@@ -437,14 +530,14 @@ let linePoint = (x1, y1, x2, y2, px, py) => {
     return true;
   }
   return false;
-};
+}
 
-let easeVector = (vec, targetVec, ease) => {
+function easeVector(vec, targetVec, ease) {
   let dx = targetVec.x - vec.x;
   let dy = targetVec.y - vec.y;
   vec = createVector(vec.x + dx * ease, vec.y + dy * ease);
   return vec;
-};
+}
 
 function debounce(func, wait) {
   let inDebounce;
